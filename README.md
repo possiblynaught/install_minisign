b3d1e25da6e46012f4526e24fa92bed1ffaf1b98
probablynaught <possiblynaught@pm.me>
2023-02-10 16:40:42 -0600

    make script execute more quietly and add README content

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+# Install minisign
+
+This can be used as a standalone script or inherited as a submodule. It will install [minisign](https://github.com/jedisct1/minisign) if it is not already installed. It will install from dnf if on a Fedora system or build minisign (and libsodium dependency) from source otherwise.
+
+## USE
+
+```bash
+wget https://raw.githubusercontent.com/possiblynaught/install_minisign/master/install_minisign.sh
+chmod +x install_minisign.sh
+./install_minisign.sh
+```
+
 ## TODO
 
 - [x] Flip to static build flags or at least have option?
--- install_minisign.sh
+++ install_minisign.sh
@@ -4,23 +4,25 @@
 #set -x
 set -Eeo pipefail
 
+##################################################
 ### Comment this line to disable static binary ###
 STATIC_BUILD=1
+##################################################
 
 # Check os
 OS_TYPE=$(hostnamectl | grep -F "Operating System:")
 
 # Check for minisign
 if command -v minisign &> /dev/null; then
-  echo "Minisign already installed, exiting:
-  $(minisign -v)"
+  echo "Minisign $(minisign -v | cut -d " " -f 2) already installed to $(which minisign)"
   exit
 elif (echo "$OS_TYPE" | grep -qF "Fedora"); then
   sudo dnf install -y minisign
-  echo "Done, installed minisign via package manager"
+  echo "Installed minisign $(minisign -v | cut -d " " -f 2) via package manager"
   exit
+else
+  echo "Minisign not found, starting to build and install from source:"
 fi
-echo "Building minisign from source..."
 
 # Install build tools
 if ! command -v cmake &> /dev/null || \
@@ -48,42 +50,47 @@ SODIUM_TAR="$SODIUM_DIR.tar.gz"
 SODIUM_SIG="$SODIUM_TAR.minisig"
 SODIUM_LINK="https://download.libsodium.org/libsodium/releases/LATEST.tar.gz"
 SODIUM_SIG_LINK="$SODIUM_LINK.minisig"
-wget "$SODIUM_LINK" -O "$SODIUM_TAR"
-wget "$SODIUM_SIG_LINK" -O "$SODIUM_SIG"
+echo "Downloading libsodium sources..."
+wget -q "$SODIUM_LINK" -O "$SODIUM_TAR"
+wget -q "$SODIUM_SIG_LINK" -O "$SODIUM_SIG"
 rm -rf "$SODIUM_DIR"
+echo "Unpacking libsodium sources..."
 tar -xf "$SODIUM_TAR" -C "$(dirname "$SODIUM_DIR")"
 cd "$SODIUM_DIR" || exit 1
 # Build libsodium
-./configure
-make
-sudo make install
+echo "Building libsodium sources..."
+./configure >/dev/null
+make >/dev/null
+echo "Installing libsodium from source..."
+sudo make install >/dev/null
 
 # Install minisign
-MINI_ZIP="/tmp/minisign.zip"
 MINI_DIR="/tmp/minisign-master"
+MINI_ZIP="/tmp/minisign.zip"
 MINI_LINK="https://github.com/jedisct1/minisign/archive/refs/heads/master.zip"
-wget "$MINI_LINK" -O "$MINI_ZIP"
+echo "Downloading minisign sources..."
+wget -q "$MINI_LINK" -O "$MINI_ZIP"
 rm -rf "$MINI_DIR"
-unzip "$MINI_ZIP" -d "$(dirname "$MINI_DIR")"
+echo "Unpacking minisign sources..."
+unzip -q "$MINI_ZIP" -d "$(dirname "$MINI_DIR")"
 rm -f "$MINI_ZIP"
 mkdir -p "$MINI_DIR/build"
 cd "$MINI_DIR/build" || exit 1
+echo "Building minisign sources..."
 if [[ "$STATIC_BUILD" -eq 1 ]]; then
-  cmake -D BUILD_STATIC_EXECUTABLES=1 ..
+  cmake -D BUILD_STATIC_EXECUTABLES=1 .. >/dev/null
 else
-  cmake ..
+  cmake .. >/dev/null
 fi
 make
-sudo make install
+echo "Installing minisign from source..."
+sudo make install >/dev/null
 
 # Notify of completion
 echo "
 --------------------------------------------------------------------------------
-Finished, minisign has been installed to: $(which minisign)
-Version:"
-minisign -v
-echo "
-Testing minisign by verifying libsodium library downloaded for this install:
+Finished, minisign $(minisign -v | cut -d " " -f 2) has been installed to: $(which minisign)
+Testing minisign by verifying the downloaded libsodium library:
 "
 minisign -VP RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3 -m "$SODIUM_TAR"
 rm -f "$SODIUM_TAR"
